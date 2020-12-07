8c71487ce5f90204865076709cde119c792c84ba
Clinton Hipple <clintondhipple@hotmail.com>
2020-12-07 16:23:43 -0600

    final0

---
+++ build/precache-manifest.2dca78abebc7d7863d7e23ec6cd19703.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ce635d6f4e9ee5dfddf5fe7744dd89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "405bf5223873b9bd9add",
+    "url": "/static/css/main.6f9238df.chunk.css"
+  },
+  {
+    "revision": "f1f72456fe382f8b375e",
+    "url": "/static/js/2.8d421dfc.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.8d421dfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "405bf5223873b9bd9add",
+    "url": "/static/js/main.1f3a3281.chunk.js"
+  },
+  {
+    "revision": "cfabed5267c41a13b59e",
+    "url": "/static/js/runtime-main.ddf8fe10.js"
+  },
+  {
+    "revision": "4ca40105569d94223c9fed9c673db97f",
+    "url": "/static/media/landingPageBackground.4ca40105.png"
+  },
+  {
+    "revision": "f41663400facdd27544799e909dfa98f",
+    "url": "/static/media/landingPageBackgroundMobile.f4166340.png"
+  },
+  {
+    "revision": "02b48c57ee99e35296f6a8103a39feae",
+    "url": "/static/media/showme.02b48c57.png"
+  }
+]);
