08cddfa4efff796df3ad731317a96b529674ba08
Clinton Hipple <clintondhipple@hotmail.com>
2020-12-12 13:33:43 -0600

    update 1

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -5,15 +5,13 @@ import mapStoreToProps from '../../redux/mapStoreToProps';
 class LandingPage extends Component {
   render() {
     return (
-      <div className="landingWrapper">
-        <div class="content1">
-          <p class="headtext1">Show up and Show out!</p>
-          <p class="bodytext1">
-            We connect high quality subcontractors with vendors all across the
-            country. Whatever your needs may be, we will assemble a team you can
-            count on.
-          </p>
-        </div>
+      <div class="content1">
+        <p class="headtext1">Show up and Show out!</p>
+        <p class="bodytext1">
+          We connect high quality subcontractors with vendors all across the
+          country. Whatever your needs may be, we will assemble a team you can
+          count on.
+        </p>
       </div>
     );
   }
