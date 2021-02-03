ec82a91ad25ddfdaedc7ef110f8d0d820b390ab2
Clinton Hipple <clintondhipple@hotmail.com>
2021-02-03 14:12:46 -0600

    using lorem ipsum no sdfsdf

--- src/components/LandingPage/AboutUs.js
+++ src/components/LandingPage/AboutUs.js
@@ -22,24 +22,31 @@ function AboutUs(props) {
           <img src={image7} className="aboutImage" />
           <h1>Ten years of results</h1>
           <p>
-            lorem ipsum sdsf sdf sdf sdfsdf wesgsg d gsd lorem ipsum sdsf sdf
-            sdf sdfsdf wesgsg d gsd{' '}
+            Exercitation labore aliquip id consequat. Aute ipsum eiusmod
+            cupidatat occaecat. Aliqua ullamco occaecat dolor ullamco nulla
+            voluptate deserunt. Aliquip laboris tempor quis excepteur laborum id
+            sint occaecat exercitation ullamco consequat dolor.
           </p>
         </Grid>
         <Grid item lg={2} sm={12} xs={12} className="aboutUsGridItem">
           <img src={image3} className="aboutImage" />
           <h1>Bringing you the best</h1>
           <p>
-            lorem ipsum sdsf sdf sdf sdfsdf wesgsg d gsd lorem ipsum sdsf sdf
-            sdf sdfsdf wesgsg d gsd{' '}
+            Ut aute dolore nisi non consectetur consequat ex proident
+            consectetur tempor non sunt duis. Occaecat aliquip tempor pariatur
+            dolore reprehenderit qui et. Sit quis irure elit tempor anim anim ex
+            ullamco. Nisi laboris officia est voluptate.
           </p>
         </Grid>
         <Grid item lg={5} sm={12} xs={12} className="aboutUsGridItem">
           <img src={image4} className="aboutImage" />
           <h1>From coast to coast</h1>
           <p>
-            lorem ipsum sdsf sdf sdf sdfsdf wesgsg d gsd lorem ipsum sdsf sdf
-            sdf sdfsdf wesgsg d gsd{' '}
+            Et dolore aute culpa quis pariatur elit. Mollit cupidatat velit
+            occaecat voluptate cillum sunt sunt magna quis reprehenderit tempor.
+            Non voluptate elit minim fugiat non adipisicing cupidatat nisi aute
+            sit exercitation. Est reprehenderit id officia proident occaecat id
+            ullamco .
           </p>
         </Grid>
       </Grid>
